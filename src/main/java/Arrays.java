7516362a070d9d48a79fd0731d34361eba378f25
Profesor <Profesor@pc-profesor>
2020-01-10 16:59:08 +0100

    Starting

---
+++ src/main/java/Arrays.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/Decimales.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Decimales {
+
+	public static void main(String[] args) {
+
+	}
+
+}
