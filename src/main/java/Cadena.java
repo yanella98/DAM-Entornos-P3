2d633b2445520cc56ac9726f427bf2630a067514
DAM_1 <DAM_1@PB05>
2020-01-31 20:23:10 +0100

    Mejora07

--- src/main/java/Cadena.java
+++ src/main/java/Cadena.java
@@ -1,6 +1,12 @@
 package main.java;
 
 public class Cadena {
+	public static String tipo(String texto) {
+		String tipo;
+		tipo = "El nombre en minúscula es: "+texto.toLowerCase()+
+			   "\nEl nombre en mayúsculas es: "+texto.toUpperCase();
+		return tipo;
+	}
 	public static String LongitudNumero(String texto) {
 		String longitud;
 		longitud = "La longitud del "+texto+" es "+texto.length();
@@ -9,6 +15,7 @@ public class Cadena {
 	public static void main(java.lang.String[] args) {
 		String nombre = "programacion";
 		System.out.println(LongitudNumero(nombre));
+		System.out.println(tipo(nombre));
 	}
 
 }
