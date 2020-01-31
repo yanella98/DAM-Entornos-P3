2874dee297efe34ab189c7c572994eefb081767a
DAM_1 <DAM_1@PB04>
2020-01-31 20:53:29 +0100

    Mejora_05 (factorial)

--- src/main/java/Numero.java
+++ src/main/java/Numero.java
@@ -1,10 +1,28 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Numero {
 
 	public static void main(String[] args) {
 		int numero = 8;
+		
+		factorial(numero);
 
+	}//MAIN
+	
+	public static void factorial(int num) {
+		
+		
+		int fact = 1;
+		
+		
+		for (int i=1; i <= num ; i++) {			
+			fact = fact * i;	
+		}
+		System.out.println("El factorial de " + num + " es " + fact);
 	}
+		
+}//CLASS
 
-}
+
