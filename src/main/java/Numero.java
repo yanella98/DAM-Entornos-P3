0f6646879a1a2ff7a7f0aa3ac57ee7ecac0920c9
DAM_1 <DAM_1@PB04>
2020-01-31 21:06:34 +0100

    mejora04 (primo)

--- src/main/java/Numero.java
+++ src/main/java/Numero.java
@@ -1,10 +1,35 @@
+
 package main.java;
+
+
 
 public class Numero {
 
 	public static void main(String[] args) {
 		int numero = 8;
+		
+		numeroPrimo(numero);
 
+	}//MAIN
+	
+	public static void numeroPrimo(int num) {	        
+	 
+	        int contador = 0;
+	 
+	        for(int I = 1; I <= num; I++)
+	        {
+	            if((num % I) == 0)
+	            {
+	                contador++;
+	            }
+	        }
+	 
+	        if(contador <= 2)
+	        {
+	            System.out.println("El numero " + num + " es primo");
+	        }else{
+	            System.out.println("El numero "+ num + " no es primo");
+	        }
 	}
-
-}
+		
+}//CLASS
