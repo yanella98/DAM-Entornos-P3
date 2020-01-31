956d5f5a56df6c836083d57be1c8d560c94f6e37
DAM_1 <DAM_1@PB05>
2020-01-31 19:54:14 +0100

    Mejora 01

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,10 +1,23 @@
 package main.java;
 
 public class Array {
+	
+	public static String NumeroMax(int []array) {
+		int mayor = array[0];
+		String max;
+		for(int i = 0; i < array.length; i++){
+			if(mayor < array[i])
+			{
+				mayor = array[i];
+			}
+		}
+		max = "El valor máximo es " + mayor;
+		return max;
+	}
 
 	public static void main(String[] args) {
 		int[]array = {5,4,8,6,3,2,9};
-
+		System.out.println(NumeroMax(array));
 		
 	}
 
