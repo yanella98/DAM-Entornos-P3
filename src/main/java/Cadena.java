7ba0107b62f21cf622902f5ec0ee585451cf1944
DAM_1 <DAM_1@PB05>
2020-01-31 20:05:16 +0100

    Mejora06

--- src/main/java/Cadena.java
+++ src/main/java/Cadena.java
@@ -1,11 +1,14 @@
 package main.java;
 
 public class Cadena {
-
+	public static String LongitudNumero(String texto) {
+		String longitud;
+		longitud = "La longitud del "+texto+" es "+texto.length();
+		return longitud;
+	}
 	public static void main(java.lang.String[] args) {
-
 		String nombre = "programacion";
-
+		System.out.println(LongitudNumero(nombre));
 	}
 
 }
